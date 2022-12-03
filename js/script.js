ebc0f8585420419bdcea1e05b943d5476919ea41
Maksim96rus <maksikk96rus@yandex.ru>
2022-12-03 14:32:49 +0500

    automatink conditions, cicles, check

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-const numberOfFilms = prompt('Сколько фильмов вы уже посмтреле?', '');
+"use strict";
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмтреле?', '');
 
 const personalMovievDB = {
     count: numberOfFilms,
@@ -7,12 +9,31 @@ const personalMovievDB = {
     genres:[],
     privat: false
 };
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
 
-personalMovievDB.movies[a] = b;
-personalMovievDB.movies[c] = d;
+for (let i=0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovievDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}      
+
+if (personalMovievDB.count < 10){
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovievDB.count >= 10 && personalMovievDB.count < 30){
+    console.log('Вы классичесский зритель'); 
+} else if (personalMovievDB.count >= 30){
+    console.log('Вы киноман'); 
+} else {
+    console.log('Произошла ошибка');
+}
+    
+
 
 console.log(personalMovievDB);
+
